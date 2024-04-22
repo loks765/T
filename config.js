28ef7081fd06daace85a9e0e05d288913899090f
SharkYTB <139496573+SharkYTB@users.noreply.github.com>
2024-04-22 19:12:46 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import { watchFile, unwatchFile } from 'fs' 
+  import { watchFile, unwatchFile } from 'fs' 
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
@@ -9,8 +9,10 @@ import moment from 'moment-timezone'
 import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js' 
 
 global.owner = [
-  ['+966 56 904 4652', '𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ', true],
-  ['+966 56 300 7262', '', false],
+  ['+966561841257', '𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ', false],
+  ['+966540235070', '𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ', true],
+  ['', '𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ', false],
+  ['', '𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ', false],
 ] //Number of owners
 
 global.mods = ['917605902011','3246024864656'] 
@@ -72,9 +74,9 @@ global.maxwarn = 3 // máxima advertencias
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
+unwatchFile(file)
+console.log(chalk.redBright("Update 'config.js'"))
+import(`${file}?update=${Date.now()}`)
 })
 
 global.lenguajeGB = es  //Idioma de The-ShadowBot-MD, Ejemplo: es | en | pt...
@@ -84,7 +86,7 @@ global.lenguajeGB = es  //Idioma de The-ShadowBot-MD, Ejemplo: es | en | pt...
 //⊱ ━━━━━.⋅ Información ⋅.━━━━ ⊰
 
 global.wm = '𝐷𝐼𝐴𝐵𝐿𝑂᯽𝐵𝑂𝑇'
-global.igfg = '𝐵𝑌:𝑺𝒉𝒂𝒓𝒌𝒚あ𝐒𝐚𝐦𝐚'
+global.igfg = '𝐵𝑌:𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ'
 global.wait = '*⌛ _انتظر لحظة..._ ▬▭▭▭▭▭▭*'
 global.waitt = '*⌛ _انتظر لحظة..._ ▬▬▭▭▭*'
 global.waittt = '*⌛ _انتظر لحظة..._ ▬▬▬▬▭▭*'
@@ -93,19 +95,167 @@ global.waitttt = '*⌛ _انتظر لحظة..._ ▬▬▬▬▬▬▭*'
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.vs = '1.4.9'
-global.lb = '𝐵𝑌:𝑺𝒉𝒂𝒓𝒌𝒚あ𝐒𝐚𝐦𝐚'
+global.lb = '𝐵𝑌:𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ'
 global.yt = ''
-global.ig = 'wa.me/966569044652'
+global.ig = 'wa.me/966540235070'
 global.md = ''
-global.fb = 'wa.me/966569044652'
+global.fb = 'wa.me/966540235070'
 
-global.nna = 'wa.me/966569044652' //تحديث ، معلومات عن الروبوت
-global.nn = 'wa.me/966569044652' // جروب 1
-global.nnn = 'wa.me/966569044652' //جروب 2
-global.nnnt = 'wa.me/966569044652' //Grupo del Colaboracion
-global.nnntt = 'wa.me/966569044652' //Grupo COL 2 
-global.nnnttt = 'wa.me/966569044652' //enlace lolibot
-global.nnntttt = 'wa.me/966569044652' //Grupo ayuda sobre el bot
-global.asistencia = 'wa.me/966569044652' //Dudas? escríbeme...G
+global.nna = 'wa.me/966540235070' //تحديث ، معلومات عن الروبوت
+global.nn = 'wa.me/966540235070' // جروب 1
+global.nnn = 'wa.me/966540235070' //جروب 2
+global.nnnt = 'wa.me/966540235070' //Grupo del Colaboracion
+global.nnntt = 'wa.me/966540235070' //Grupo COL 2 
+global.nnnttt = 'wa.me/966540235070' //enlace lolibot
+global.nnntttt = 'wa.me/966540235070' //Grupo ayuda sobre el bot
+global.asistencia = 'wa.me/966540235070' //Dudas? escríbeme...G
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+//⊱ ━━━━━━━━━━━━━.⋅ Datos ⋅.━━━━━━━━━━━━━━ ⊰
+
+global.rg = '*╰⊱✅⊱الــنــتــائــج✅⊱╮*\n'
+global.resultado = rg
+global.ag = '*╰⊱⚠️⊱تــحــذيــر⚠️⊱╮*\n'
+global.advertencia = ag
+global.iig = '*╰⊱❕⊱مــعــلــومــات⊱⊱╮*\n'
+global.informacion = iig
+global.fg = '*╰⊱❌خــطــأ❌⊱╮**\n'
+global.fallo = fg
+global.mg = '*╰⊱❗️⊱اســتخــدام خــاطــئ❗️⊱╮*\n'
+global.mal = mg
+global.eeg = '*╰⊱📩⊱تــقــريــر📩⊱╮*\n'
+global.envio = eeg
+global.eg = '*╰⊱💚خــروج💚⊱╮*\n'
+global.exito = eg
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+//━━━━━━━━━━━━━━ img ━━━━━━━━━
+
+global.img4 = 'https://telegra.ph/file/1c0df3c550addd14ef58c.jpg' //prem
+
+global.img5 = ''
+global.img6 = ''
+global.img7 = ''
+global.img8 = ''
+global.img9 = ''
+
+global.img10 = 'https://telegra.ph/file/8c0bfff623ca0a5582710.jpg'
+global.img11 = 'https://telegra.ph/file/5adbd7bfb6afa40ce5045.jpg'
+global.img12 = 'https://telegra.ph/file/86f4d8e6bfb7ba044994f.jpg'
+global.img13 = 'https://telegra.ph/file/6ed2ee2384f8a505966ee.jpg'
+global.img14 = 'https://telegra.ph/file/6409fc05085b0b47edb4c.jpg'
+global.img15 = 'https://telegra.ph/file/d7ae77d1178f9de50825c.jpg'
+
+global.img16 = 'https://telegra.ph/file/86f4d8e6bfb7ba044994f.jpg' //+18
+
+global.img17 = 'https://telegra.ph/file/6409fc05085b0b47edb4c.jpg'
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+//⊱ ━━━━━.⋅ RPG ⋅.━━━━ ⊰
+
+global.flaaa = [
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
+
+global.cmenut = '❖––––––『'
+global.cmenub = '┊✦ '
+global.cmenuf = '╰━═┅═━––––––๑\n'
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     '
+
+global.dmenut = '*❖─┅──┅〈*'
+global.dmenub = '*┊»*'
+global.dmenub2 = '*┊*'
+global.dmenuf = '*╰┅────────┅✦*'
+global.htjava = '⫹⫺'
+
+global.htki = '*⭑•̩̩͙⊱•••• ☪*'
+global.htka = '*☪ ••••̩̩͙⊰•⭑*'
+
+global.comienzo = '• • ◕◕════'
+global.fin = '════◕◕ • •'
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+global.rpgshop = { //Tienda
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emottt = {
+      exp: lenguajeGB.eExp(), limit: lenguajeGB.eDiamante(), diamond: lenguajeGB.eDiamantePlus(), joincount: lenguajeGB.eToken(),
+      emerald: lenguajeGB.eEsmeralda(), berlian: lenguajeGB.eJoya(), kyubi: lenguajeGB.eMagia(), gold: lenguajeGB.eOro(),
+      money: lenguajeGB.eGataCoins(), tiketcoin: lenguajeGB.eGataTickers(), stamina: lenguajeGB.eEnergia(),
+
+      potion: lenguajeGB.ePocion(), aqua: lenguajeGB.eAgua(), trash: lenguajeGB.eBasura(), wood: lenguajeGB.eMadera(),
+      rock: lenguajeGB.eRoca(), batu: lenguajeGB.ePiedra(), string: lenguajeGB.eCuerda(), iron: lenguajeGB.eHierro(),
+      coal: lenguajeGB.eCarbon(), botol: lenguajeGB.eBotella(), kaleng: lenguajeGB.eLata(), kardus: lenguajeGB.eCarton(),
+
+      eleksirb: lenguajeGB.eEletric(), emasbatang: lenguajeGB.eBarraOro(), emasbiasa: lenguajeGB.eOroComun(), rubah: lenguajeGB.eZorroG(),
+      sampah: lenguajeGB.eBasuraG(), serigala: lenguajeGB.eLoboG(), kayu: lenguajeGB.eMaderaG(), sword: lenguajeGB.eEspada(),
+      umpan: lenguajeGB.eCarnada(), healtmonster: lenguajeGB.eBillete(), emas: lenguajeGB.ePinata(), pancingan: lenguajeGB.eGancho(),
+      pancing: lenguajeGB.eCanaPescar(),
+
+      common: lenguajeGB.eCComun(), uncoommon: lenguajeGB.ePComun(), mythic: lenguajeGB.eCMistica(),
+      pet: lenguajeGB.eCMascota(),//?
+      gardenboxs: lenguajeGB.eCJardineria(),//?
+      legendary: lenguajeGB.eClegendaria(),
+
+      anggur: lenguajeGB.eUva(), apel: lenguajeGB.eManzana(), jeruk: lenguajeGB.eNaranja(), mangga: lenguajeGB.eMango(), pisang: lenguajeGB.ePlatano(),
+
+      bibitanggur: lenguajeGB.eSUva(), bibitapel: lenguajeGB.eSManzana(), bibitjeruk: lenguajeGB.eSNaranja(), bibitmangga: lenguajeGB.eSMango(), bibitpisang: lenguajeGB.eSPlatano(),
+
+      centaur: lenguajeGB.eCentauro(), griffin: lenguajeGB.eAve(), kucing: lenguajeGB.eGato(), naga: lenguajeGB.eDragon(),
+      fox: lenguajeGB.eZorro(), kuda: lenguajeGB.eCaballo(), phonix: lenguajeGB.eFenix(), wolf: lenguajeGB.eLobo(),
+      anjing: lenguajeGB.ePerro(),
+
+      petFood: lenguajeGB.eAMascots(), //?
+      makanancentaur: lenguajeGB.eCCentauro(), makanangriffin: lenguajeGB.eCAve(),
+      makanankyubi: lenguajeGB.eCMagica(), makanannaga: lenguajeGB.eCDragon(), makananpet: lenguajeGB.eACaballo(), makananphonix: lenguajeGB.eCFenix()
+    }
+    let results = Object.keys(emottt).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emottt[results[0][0]]
+  }
+}
+
+global.rpgshopp = { //Tienda
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emotttt = {
+      exp: '⚡', limit: '💎', diamond: '💎+', joincount: '🪙',
+      emerald: '💚', berlian: '♦️', kyubi: '🌀', gold: '👑',
+      money: '🐱', tiketcoin: '🎫', stamina: '✨',
+
+      potion: '🥤', aqua: '💧', trash: '🗑', wood: '🪵',
+      rock: '🪨', batu: '🥌', string: '🕸️', iron: '⛓️',
+      coal: '⚱️', botol: '🍶', kaleng: '🥫', kardus: '🪧',
+
+      eleksirb: '💡', emasbatang: '〽️', emasbiasa: '🧭', rubah: '🦊🌫️',
+      sampah: '🗑🌫️', serigala: '🐺🌫️', kayu: '🛷', sword: '⚔️',
+      umpan: '🪱', healtmonster: '💵', emas: '🪅', pancingan: '🪝',
+      pancing: '🎣',
+
+      common: '📦', uncoommon: '🥡', mythic: '🗳️',
+      pet: '📫',//?
+      gardenboxs: '💐',//?
+      legendary: '🎁',
+
+      anggur: '🍇', apel: '🍎', jeruk: '🍊', mangga: '🥭', pisang: '🍌',
+
+      bibitanggur: '🌾🍇', bibitapel: '🌾🍎', bibitjeruk: '🌾🍊', bibitmangga: '🌾🥭', bibitpisang: '🌾🍌',
+
+      centaur: '🐐', griffin: '🦅', kucing: '🐈', naga: '🐉', fox: '🦊', kuda: '🐎', phonix: '🕊️', wolf: '🐺', anjing: '🐶',
+
+      petFood: '🍖', //?
+      makanancentaur: '🐐🥩', makanangriffin: '🦅🥩', makanankyubi: '🌀🥩', makanannaga: '🐉🥩',
+      makananpet: '🍱🥩', makananphonix: '🕊️🥩'  
+    }
+    let results = Object.keys(emotttt).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emotttt[results[0][0]]
+  }
+}
